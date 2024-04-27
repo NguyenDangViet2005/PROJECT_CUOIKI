d26b990a86d2663d75aa44c2e94db48e1fa32a06
NguyenDangViet2005 <vietnguyen.1022005@gmail.com>
2024-04-27 19:43:33 +0700

    reviews

--- js/main.js
+++ js/main.js
@@ -1,79 +1,96 @@
 //phần dropdown
-var menu = document.getElementById('menu');
-        var submenu = document.getElementById('submenu');
+var menu = document.getElementById("menu");
+var submenu = document.getElementById("submenu");
 
-        menu.addEventListener('click', function() {
-            submenu.classList.toggle('open');
-        });
+menu.addEventListener("click", function () {
+  submenu.classList.toggle("open");
+});
 //phần view more
 var viewmoreBtn = document.querySelector(".view-more");
 var currentBox = 6;
-viewmoreBtn.onclick = function(){
-    let boxes = [...document.querySelectorAll('.model .box-model')];
-    for(var i=currentBox ; i< currentBox+2 ; i++){
-        boxes[i].style.display = 'inline-block';
-    }
-    currentBox +=2;
-    if(currentBox >= boxes.length){
-        viewmoreBtn.style.display='none';
-    }
-}
+viewmoreBtn.onclick = function () {
+  let boxes = [...document.querySelectorAll(".model .box-model")];
+  for (var i = currentBox; i < currentBox + 2; i++) {
+    boxes[i].style.display = "inline-block";
+  }
+  currentBox += 2;
+  if (currentBox >= boxes.length) {
+    viewmoreBtn.style.display = "none";
+  }
+};
 
 // phần slider
-let list = document.querySelector('.slider .list');
-let item = document.querySelectorAll('.slider .list .list-item');
-let dots = document.querySelectorAll('.slider .dots li');
-let prev = document.getElementById('prev');
-let next = document.getElementById('next');
+let list = document.querySelector(".slider .list");
+let item = document.querySelectorAll(".slider .list .list-item");
+let dots = document.querySelectorAll(".slider .dots li");
+let prev = document.getElementById("prev");
+let next = document.getElementById("next");
 
 let active = 0;
 let lengthItem = item.length - 1;
 let refreshslider;
 
 next.onclick = function () {
-    if (active + 1 > lengthItem) {
-        active = 0;
-    } else {
-        active = active + 1;
-    }
-    reloadslider();
+  if (active + 1 > lengthItem) {
+    active = 0;
+  } else {
+    active = active + 1;
+  }
+  reloadslider();
 };
 prev.onclick = function () {
-    if (active - 1 < 0) {
-        active = lengthItem;
-    } else {
-        active = active - 1;
-    }
-    reloadslider();
+  if (active - 1 < 0) {
+    active = lengthItem;
+  } else {
+    active = active - 1;
+  }
+  reloadslider();
 };
-refreshslider = setInterval(() => { next.click(); }, 3000);
+refreshslider = setInterval(() => {
+  next.click();
+}, 3000);
 
 function reloadslider() {
-    let checkleft = item[active].offsetLeft;//*
-    list.style.left = -checkleft + 'px';//*
+  let checkleft = item[active].offsetLeft; //*
+  list.style.left = -checkleft + "px"; //*
 
-    let lastActiveDot = document.querySelector('.slider .dots li.active');
-    lastActiveDot.classList.remove('active');
-    dots[active].classList.add('active');
+  let lastActiveDot = document.querySelector(".slider .dots li.active");
+  lastActiveDot.classList.remove("active");
+  dots[active].classList.add("active");
 
-    if (refreshslider) {
-        clearInterval(refreshslider);//*
-        refreshslider = setInterval(() => { next.click(); }, 3000);
-    }
+  if (refreshslider) {
+    clearInterval(refreshslider); //*
+    refreshslider = setInterval(() => {
+      next.click();
+    }, 3000);
+  }
 }
 
 dots.forEach((li, key) => {
-    li.addEventListener('click', function () {
-        active = key;
-        reloadslider();
-    });
+  li.addEventListener("click", function () {
+    active = key;
+    reloadslider();
+  });
 });
 //tech slide
-document.getElementById('tech-next').onclick=function(){
-    let lists = document.querySelectorAll('.tech-item');
-    document.getElementById('tech-slide').appendChild(lists[0]);
-}
-document.getElementById('tech-prev').onclick=function(){
-    let lists = document.querySelectorAll('.tech-item');
-    document.getElementById('tech-slide').prepend(lists[lists.length-1]);
-}
+document.getElementById("tech-next").onclick = function () {
+  let lists = document.querySelectorAll(".tech-item");
+  document.getElementById("tech-slide").appendChild(lists[0]);
+};
+document.getElementById("tech-prev").onclick = function () {
+  let lists = document.querySelectorAll(".tech-item");
+  document.getElementById("tech-slide").prepend(lists[lists.length - 1]);
+};
+//view more comment
+var viewmoreBtnComment = document.querySelector(".review-view-more");
+var currentBoxComment = 6;
+viewmoreBtnComment.onclick = function () {
+  let boxesComment = [...document.querySelectorAll(".reviews .user")];
+  for (var i = currentBoxComment; i < currentBoxComment + 2; i++) {
+    boxesComment[i].style.display = "flex";
+  }
+  currentBoxComment += 2;
+  if (currentBoxComment >= boxesComment.length) {
+    viewmoreBtnComment.style.display = "none";
+  }
+};
