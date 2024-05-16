1684ee6b58f4fa7cbe3e3dba409fcb11dca4e527
NguyenDangViet2005 <vietnguyen.1022005@gmail.com>
2024-05-16 23:10:14 +0700

    model change

--- js/model.js
+++ js/model.js
@@ -4,3 +4,13 @@ var submenu = document.getElementById("submenu");
 menu.addEventListener("click", function () {
   submenu.classList.toggle("open");
 });
+
+var slide = document.querySelector(".slide-furniture");
+var slide_item = document.querySelectorAll(".slide-furniture .furniture-item");
+
+setInterval(() => {
+  var first_item = document.querySelector(
+    ".slide-furniture .furniture-item:first-child"
+  );
+  slide.appendChild(first_item);
+}, 3000);
